60ba79796e824788e09b915354bca84d171e06f0
Wojciech Rosa <wojciech.rosa@gmail.com>
2019-07-10 21:00:56 +0200

    Update irr_config.py

--- irr_config.py
+++ irr_config.py
@@ -4,7 +4,7 @@ gpio_pump = 17
 gpio_rain_test = 2
 gpio_water_test = 3
 gpio_main_switch = 5
-gpio_sections_list =[5, 6, 13, 19,22, 26]
+gpio_sections_list =[13, 6, 5, 19,22, 26]
 
 file_test_log="/home/pi/irr3/water_test.log"
 file_action_log="/home/pi/irr3/water_pump.log"
